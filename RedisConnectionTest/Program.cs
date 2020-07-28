d673a3813355feebc8143ea50fba2d8719c94ac5
Nayanava De <nayde@microsoft.com>
2020-07-29 01:16:29 +0530

    Initial commit

---
+++ RedisConnectionTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using StackExchange.Redis;
+
+namespace RedisConnectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            CancellationTokenSource cts = new CancellationTokenSource();
+            RedisConnectorHelper rch = new RedisConnectorHelper(cts);
+
+            Console.WriteLine("Writing to redis");
+            program.SaveBigData(rch);
+            Console.WriteLine("Reading from cache");
+            program.ReadData(rch);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Leemos los 10,000 datos guardados
+        /// </summary>
+        public void ReadData(RedisConnectorHelper cache)
+        {
+            var devicesCount = 2;
+            for (int i = 1; i <= devicesCount; i++)
+            {
+                string key = $"Device_Status:{i}";
+                var value = cache.StringGet(key);
+
+                Console.WriteLine($"Key = {key}, Value = {value}");
+            }
+        }
+
+
+        /// <summary>
+        /// Guarda valores random en 10000 dispositivos Dummy
+        /// </summary>
+        /// <returns></returns>
+        public void SaveBigData(RedisConnectorHelper cache)
+        {
+            var devicesCount = 2;
+            var rnd = new Random();
+
+            for (int i = 1; i <= devicesCount; i++)
+            {
+                string key = $"Device_Status:{i}";
+                var value = rnd.Next(0, 100);
+                cache.StringSet(key, value.ToString());
+                Console.WriteLine($"Key = {key}, Value = {value}");
+            }
+        }
+    }
+}
---
+++ RedisConnectionTest/RedisConnectorHelper.cs
@@ -0,0 +1,43 @@
+﻿using Firehose.Cache.Distributed.Redis;
+using RedisShardingBundle;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RedisConnectionTest
+{
+    public class RedisConnectorHelper
+    {
+        private readonly CancellationTokenSource CancellationTokenSource;
+        private readonly RedisConnectionsManager redisConnectionsManager;
+        public RedisConnectorHelper(CancellationTokenSource cancellationTokenSource)
+        {
+            CancellationTokenSource = cancellationTokenSource;
+            redisConnectionsManager = initRedisConnectionManager();
+            
+        }
+
+        private RedisConnectionsManager initRedisConnectionManager() {
+            IList<RedisConnectionConfig> connectionConfigs = new List<RedisConnectionConfig>
+            {
+                new RedisConnectionConfig("splicluster1.redis.cache.windows.net:6380,password=RrRDwXnjXqLeGTMkMNDfHyFLa1UPXSMjbOW7amRSnZQ=,ssl=True,abortConnect=False", 1),
+                new RedisConnectionConfig("splitcluster2.redis.cache.windows.net:6380,password=DZEMUVwxoSdqL0xFhlot3jq+43v30QOERVnT7mXePao=,ssl=True,abortConnect=False", 1)
+            };
+            RedisConnectionsManager rcm = new RedisConnectionsManager(32, 1, 2, 1, connectionConfigs.ToArray(), CancellationTokenSource.Token);
+            return rcm;
+        }
+
+        public bool StringSet(string key, string value)
+        {
+            return redisConnectionsManager.StringSet(key, key, value, TimeSpan.FromMinutes(2));
+        }
+
+        public string StringGet(string key)
+        {
+            RedisValue redisValue = redisConnectionsManager.StringGet(key, key);
+            return redisValue.ToString();
+        }
+    }
+}
